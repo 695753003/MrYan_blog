e0497dd4173dfd99aab1235c8aa6af86a68c3be8
695753003 <695753003@qq.com>
2019-03-20 19:27:31 +0800

    smtp update

--- application/extra/smtp.php
+++ application/extra/smtp.php
@@ -3,6 +3,6 @@ return [
 'email_server'=>"smtp.qq.com", //SMTP服务器地址
 'encryption'=>"ssl", //加密方式
 'port'=>"465", //端口号
-'send_user'=>"695753003@qq.com", //邮箱账号
-'smtp_pwd'=>"lfulafjlkkdtbfga", //SMTP授权密码
+'send_user'=>"xxxxxx@qq.com", //邮箱账号
+'smtp_pwd'=>"xxxxxxx", //SMTP授权密码
 ];
